805a42d4824924af5c8a21c48826bd8b48563a57
SHUBHAM SHUKLA <shubhamshu382@gmail.com>
2026-07-12 00:47:28 +0530

    Time: 0 ms (100%), Space: 9 MB (18.51%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool isValid(string s) {
+
+        int n = s.size();
+        stack<char> stk;
+
+        for(int i = 0; i < n; i++) {
+
+            if(s[i] == '(' || s[i] == '{' || s[i] == '[') {
+                stk.push(s[i]);
+            }
+            else {
+
+                if(stk.empty()) {
+                    return false;
+                }
+
+                if(stk.top() == '(' && s[i] == ')') {
+                    stk.pop();
+                }
+                else if(stk.top() == '{' && s[i] == '}') {
+                    stk.pop();
+                }
+                else if(stk.top() == '[' && s[i] == ']') {
+                    stk.pop();
+                }
+                else {
+                    return false;
+                }
+            }
+        }
+
+        return stk.empty();
+    }
+};
