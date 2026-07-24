3482b4ef01c9c2ef0b7ca129802bc8859e76474a
SHUBHAM SHUKLA <shubhamshu382@gmail.com>
2026-07-24 18:32:30 +0530

    Time: 0 ms (100%), Space: 15.3 MB (36.37%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -3,13 +3,43 @@ public:
     int search(vector<int>& nums, int target) {
 
         int n=nums.size();
-        int ans=-1;
-        for(int i=0; i<n;i++){
-            if(nums[i]==target){
-                ans=i;
-                
+        int low=0;
+        int high=n-1;
+        int res=-1;
+
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(nums[mid]==target){
+                res=mid;
+                return res;
             }
-        }return ans;
+            if(nums[mid]>nums[n-1]){
+                if(nums[mid]<target){
+                    low=mid+1;
+                }else{
+                    if(nums[0]>target){
+                        low=mid+1;
+                    }else{
+                        high=mid-1;
+                        
+                    }
+                
+                }
+            }
+            else{
+                if(nums[mid]>target){
+                    high=mid-1;
+
+                }else{
+                    if(nums[n-1]<target){
+                        high=mid-1;
+                    }else{
+                        low=mid+1;   
+                    }
+                }
+            }
+            
+        }return res;
         
     }
 };
