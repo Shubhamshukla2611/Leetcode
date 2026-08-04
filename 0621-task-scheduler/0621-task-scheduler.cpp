ab123025c108fdf6c46e51e95896a05af65b3e54
SHUBHAM SHUKLA <shubhamshu382@gmail.com>
2026-08-04 23:40:40 +0530

    Time: 4 ms (69.33%), Space: 38.2 MB (84.43%) - LeetHub

--- 0621-task-scheduler/0621-task-scheduler.cpp
+++ 0621-task-scheduler/0621-task-scheduler.cpp
@@ -3,51 +3,31 @@ public:
     int leastInterval(vector<char>& tasks, int n) {
 
         unordered_map<char,int> freq;
+        int m=tasks.size();
 
-        for(char ch : tasks){
-            freq[ch]++;
+        for(int i=0; i<m;i++){
+            freq[tasks[i]]++;
         }
 
-        priority_queue<int> pq;
+        int maxfreq=0;
+        for(auto i: freq){
+            maxfreq=max(maxfreq,i.second);
 
-        for(auto it : freq){
-            pq.push(it.second);
+        }
+        int maxcount=0;
+
+        for(auto i : freq){
+            if(i.second==maxfreq){
+                maxcount++;
+            }
         }
 
-        int time = 0;
+        int ans=(maxfreq-1)*(n+1)+maxcount;
+        
 
-        while(!pq.empty()){
+        return max(m, ans);
 
-            vector<int> temp;
 
-            int cycle = n + 1;
 
-            while(cycle > 0 && !pq.empty()){
-
-                int curr = pq.top();
-                pq.pop();
-
-                curr--;
-
-                if(curr > 0){
-                    temp.push_back(curr);
-                }
-
-                time++;
-                cycle--;
-            }
-
-            for(int x : temp){
-                pq.push(x);
-            }
-
-            if(pq.empty()){
-                break;
-            }
-
-            time += cycle;
-        }
-
-        return time;
     }
 };
