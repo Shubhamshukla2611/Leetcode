912b1cf0ea9cd45940fe0fb5952401e35715aa3d
SHUBHAM SHUKLA <shubhamshu382@gmail.com>
2026-08-04 13:39:59 +0530

    Time: 16 ms (12.57%), Space: 20.1 MB (10.5%) - LeetHub

--- 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.cpp
+++ 0378-kth-smallest-element-in-a-sorted-matrix/0378-kth-smallest-element-in-a-sorted-matrix.cpp
@@ -1,40 +1,26 @@
-class Solution {
-public:
-int fun(vector<vector<int>>& matrix, int n , int m, int guess){
-     int row=n-1;
-     int col=0;
-     int count=0;
-     while(row>=0 && col<m){
-        if(matrix[row][col]<=guess){
-            count=count+row+1;
-            col++;
-
-            
-        }else{
-            row--;
-        }
-     }return count;
-}
-    int kthSmallest(vector<vector<int>>& matrix, int k) {
-
-        int n= matrix.size();
+class Solution{
+    public:
+    int kthSmallest(vector<vector<int>>& matrix, int k){
+        int n=matrix.size();
         int m=matrix[0].size();
-        int low=matrix[0][0];
-        int high=matrix[n-1][m-1];
-        int res=-1;
+        priority_queue<int,vector<int>,greater<int>>pq;
+        vector<int>res;
 
-        while(low<=high){
-            int mid=(low+high)/2;
-            int ans=fun(matrix,n,m,mid);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                int a=matrix[i][j];
+                pq.push(a);
+            }
+           
+        }
 
-            if(ans<k){
-                low=mid+1;
-            }
-            else{
-                res=mid;
-                high=mid-1;
-            }
-        }return res;
-        
+        while(!pq.empty()){
+            res.push_back(pq.top());
+            pq.pop();
+
+
+        }return res[k-1];
+
+
     }
 };
