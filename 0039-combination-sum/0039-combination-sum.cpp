1d80ffca7bae0d7223c66eae2849c2037f10374e
SHUBHAM SHUKLA <shubhamshu382@gmail.com>
2026-08-09 16:41:02 +0530

    Time: 3 ms (52.32%), Space: 18 MB (21.53%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+
+void fun(vector<int>& candidates, int n, int idx, vector<int>& temp,int sum, vector<vector<int>>& res,int target){
+
+
+    if(idx==n){
+        return;
+    }
+    if(sum==target){
+        res.push_back(temp);
+        return;
+    }
+
+    
+
+        // nhi lenge 
+
+        fun(candidates,n,idx+1, temp,sum,res,target);
+
+        // lenge 
+        if(candidates[idx]+sum<=target){
+            temp.push_back(candidates[idx]);
+            sum+=candidates[idx];
+            fun(candidates,n,idx,temp,sum,res,target);
+            temp.pop_back();
+        }return;
+
+  
+
+
+}
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+
+        vector<int> temp;
+        vector<vector<int>> res;
+
+        fun(candidates,candidates.size(),0,temp,0,res,target);
+        return res;
+        
+    }
+};
