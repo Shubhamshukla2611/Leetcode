7104d703011b9ba6a279f4cb002604ff548fec71
SHUBHAM SHUKLA <shubhamshu382@gmail.com>
2026-08-19 23:42:38 +0530

    Time: 0 ms (100%), Space: 27.5 MB (58.68%) - LeetHub

---
+++ 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -0,0 +1,46 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+
+    unordered_map<int, int> in;
+    int idx;
+
+    TreeNode* fun(vector<int>& postorder, int low, int high) {
+
+        if(low > high) {
+            return NULL;
+        }
+
+        TreeNode* ans = new TreeNode(postorder[idx]);
+        idx--;
+
+        int id = in[ans->val];
+
+        ans->right = fun(postorder, id + 1, high);
+        ans->left = fun(postorder, low, id - 1);
+
+        return ans;
+    }
+
+    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
+
+        for(int i = 0; i < inorder.size(); i++) {
+            in[inorder[i]] = i;
+        }
+
+        idx = postorder.size() - 1;
+
+        return fun(postorder, 0, inorder.size() - 1);
+    }
+};
