7c623721d9688a3c2b796d61d6b196580fd1d78f
SHUBHAM SHUKLA <shubhamshu382@gmail.com>
2026-09-23 00:29:38 +0530

    Time: 0 ms (100%), Space: 87.1 MB (75.86%) - LeetHub

---
+++ 0860-lemonade-change/0860-lemonade-change.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+
+        int n = bills.size();
+
+        int five = 0;
+        int ten = 0;
+
+        for(int i = 0; i < n; i++) {
+
+            int money = bills[i];
+
+            if(money == 5) {
+                five++;
+            }
+
+            else if(money == 10) {
+
+                if(five == 0) {
+                    return false;
+                }
+
+                five--;
+                ten++;
+            }
+
+            if(money == 20) {
+
+                if(ten > 0 && five > 0) {
+                    ten--;
+                    five--;
+                }
+
+                else if(five >= 3) {
+                    five -= 3;
+                }
+
+                else {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+};
